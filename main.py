40d4129e1a1bf2b06adc24449e3d5a6d8ffcd623
Timothy <118371200+HilthonTT@users.noreply.github.com>
2024-09-01 14:49:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+def write_your_name():
+  name = "Arthur Morgan"
+  hobby = "robbing banks"
+
+  print(f"{name} loves {name}")
+
 def main():
   is_print = True
 
   if is_print:
     print("Hello world")
+
+  write_your_name()
 
 if __name__ == "__main__":
   main()
